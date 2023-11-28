cd6c6c3640d8f87ffcc354e5ae812c4fa7226866
salmachaveznavarro <89877537+salmachaveznavarro@users.noreply.github.com>
2023-11-28 02:42:57 -0600

    Create agregar.php
    crear agregar

---
+++ agregar.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>AGREGAR</title>
+    </head>
+    <body>
+        <h1>Agregar nuevo empleado</h1>
+        <form action="" method="post">
+            <input type="text" name="no.control"> <br>
+            <input type="submit" name="enviar" value="AGREGAR">
+            <a href="index.php"></a>
+        </form>
+    </body>
+</html>
