46bdb75f52c897055b39b0eed11739ebf65b45a8
salmachaveznavarro <89877537+salmachaveznavarro@users.noreply.github.com>
2023-11-30 13:11:22 -0600

    para eliminar

---
+++ eliminar.php
@@ -0,0 +1,19 @@
+<?php
+    $id=$_GET['id'];
+    include("conexion.php");
+
+    $sql="delete from Empleados where id='".$id."'";
+    $resultado=mysqli_query($conexion,$sql);
+
+    if($resultado){
+        echo "<script laguage= 'JavaScript'>
+                alert('Los datos se eliminaron correctamente de la BD');
+                location.assign('index.php');
+                </script>";
+    }else{
+        echo "<script laguage='JavaScript'>
+                alert('Los datos NO se eliminaron de la BD');
+                location.assign('index.php');
+                </script>";
+    }
+?>
