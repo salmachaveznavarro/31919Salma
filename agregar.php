1cdae0c4ee2bde9dffd368775fab25a559d782f7
salmachaveznavarro <89877537+salmachaveznavarro@users.noreply.github.com>
2023-11-30 13:14:05 -0600

    actualizando sgregar

--- agregar.php
+++ agregar.php
@@ -2,21 +2,38 @@
     <head>
         <title>AGREGAR</title>
     </head>
-    <body
-         <?php
-            if(isset($_POST['enviar'])){
+    <body>
+        <?php
+            if(isset($_POST['ENVIAR'])){
                 $nombre=$_POST['nombre'];
                 $nocontrol=$_POST['no.control'];
-                include("conexion.php")
+                include("conexion.php");
+                $sql="insert into Empleados(nombre,no.control) values ('".$nombre."','".$nocontrol."')";
+                $resultado=mysqli_query($conexion,$sql);
+                if ($resultado){
+                    echo "<script language='JavaScript'> alert('Los datos fueron ingresados correctamente a la BD');
+                    location.assign('index.php');
+                    </script>";
+                }else{
+                    echo "<script language='JavaScript'> alert('ERROR: Los datos NO fueron ingresados a la BD');
+                    location.assign('index.php');
+                    </script>";
+                }
+                mysqli_close($conexion);
             }else{
 
-            }
         ?>
         <h1>Agregar nuevo empleado</h1>
-        <form action="" method="post">
-            <input type="text" name="no.control"> <br>
-            <input type="submit" name="enviar" value="AGREGAR">
-            <a href="index.php"></a>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post" method= "post">
+            <label>Nombre:</label>
+            <input type="text" name="nombre"><br>
+            <label>No. Control</label>
+            <input type="text" name="no.control"><br>
+            <input type="submit" name= "enviar" value="AGREGAR">
+            <a href="index.php">Regresar</a>
         </form>
+        <?php
+            }
+        ?>
     </body>
 </html>
